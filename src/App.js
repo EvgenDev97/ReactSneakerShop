92cf753993213125026e6107d2b057c02d61cd80
Evgen <evgen.antonov.2017@mail.ru>
2023-03-14 19:40:00 +0300

    props created

--- src/App.js
+++ src/App.js
@@ -3,6 +3,30 @@ import Card from "./components/Card/Card";
 import Drawer from "./components/Drawer/Drawer";
 import Header from "./components/Header/Header";
 import Content from "./components/MainContent/Content";
+
+const arr = [
+  {
+    name: ["Men's sneaker", <br />, "Nike Blazer Mid Suede"],
+    price: `184.3 "$"`,
+    imageUrl: "/img/sneakers/jpg1.jpg",
+  },
+  {
+    name: ["Men's sneaker", <br />, "Nike Air Max 270"],
+    price: `192.3 "$"`,
+    imageUrl: "/img/sneakers/jpg2.jpg",
+  },
+  {
+    name: ["Men's sneaker", <br />, "Nike Blazer Mid Suede"],
+    price: `132.4 "$"`,
+    imageUrl: "/img/sneakers/jpg3.jpg",
+  },
+  {
+    name: ["Men`s sneaker Puma X ", <br />, " Aka Boku Future Rider"],
+    price: `135.4 "$"`,
+    imageUrl: "/img/sneakers/jpg4.jpg",
+  },
+];
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -11,10 +35,14 @@ function App() {
       <main>
         <Content />
         <div className="sneakers ">
-          <Card />
-          <Card />
-          <Card />
-          <Card />
+          {arr.map((obj) => (
+            <Card
+              title={obj.name}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => alert("item added to cart")}
+            />
+          ))}
         </div>
       </main>
     </div>
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,23 +1,21 @@
 import React from "react";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
       <div className="favorite">
         <img src="img/heart-unliked.svg" alt="unliked" />
       </div>
-      <img src="/img/sneakers/jpg1.jpg" width={133} height={112} alt="" />
-      <h5>
-        Men's sneaker <br /> Nike Blazer Mid Suede
-      </h5>
+      <img src={props.imageUrl} width={133} height={112} alt="" />
+      <h5>{props.title}</h5>
       <div className="j-flex">
         <div>
           <span>Price: </span>
           <div>
-            <b>184.3$</b>
+            <b>{props.price}</b>
           </div>
         </div>
-        <button className="button">
+        <button className="button" onClick={props.onClick}>
           <img src="/img/plus.svg" alt="plus" width={32} height={32} />
         </button>
       </div>
